5e3f2b69e59c0d6e26c335f25c126e249fc0aac5
Matteo DeSimone <thematteod@gmail.com>
2022-08-19 19:55:27 -0400

    Initial commit

---
+++ OperatorsExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorsExercises
+{
+    internal class Program
+    {
+        public static double AreaOfCircle(double r)
+        {
+            return Math.PI * (r * r);
+        }
+        static void Main(string[] args)
+        {
+
+            // Exercise 1
+            var a = 17;
+            var b = 4;
+
+
+            var div = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} divided by {b} = {div} with remainder {remainder}");
+
+            //Exercise 2
+            Console.WriteLine("What is the radius of your circle:");
+            var r = double.Parse(Console.ReadLine());
+            var areaOfCircle = AreaOfCircle(r);
+
+            Console.WriteLine($"A circle with a radius of {r} has an area of {areaOfCircle}");
+
+
+        }
+    }
+}
